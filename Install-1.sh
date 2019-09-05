7b018d2838b69efec7cc7cbbef2a129bc908a30c
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 01:19:40 +0930

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -5,7 +5,6 @@ sed -i '/^#Port 22/s/#Port 22/Port 3984/' /etc/ssh/sshd_config && sed -i '/^Perm
 adduser admin
 usermod -aG sudo admin
 sudo service sshd restart
-cd
 sudo apt-get install ufw -y
 sudo ufw allow ssh/tcp
 sudo ufw limit ssh/tcp
@@ -16,6 +15,7 @@ sudo ufw enable
 sudo apt -y install fail2ban
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
+cd
 wget https://github.com/Remmeauth/remprotocol/releases/download/0.1.0/remprotocol_0.1.0-ubuntu-18.04_amd64.deb && sudo apt install ./remprotocol_0.1.0-ubuntu-18.04_amd64.deb
 wget https://testchain.remme.io/genesis.json
 mkdir data && mkdir config
