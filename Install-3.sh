02dadd9cc9dd942072d21149cf933580d527698c
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 18:24:34 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -4,11 +4,11 @@ remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 remvault &
 sleep 10
 remcli wallet create --file walletpass
-remcli create key --file requestkey
 remcli create key --file producerkey
-cat ./requestkey
-remcli wallet import
 cat ./producerkey
+remcli wallet import
+remcli create key --file requestkey
+cat ./requestkey
 remcli wallet import
 echo "What's your account name?"
 read -e produceraccountname
