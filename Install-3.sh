0106031e3845be6256662cf6025a0606d9357d46
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-07 01:04:52 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -13,34 +13,34 @@ remcli create key --to-console
 echo "Copy and paste your active private key:"
 remcli wallet import
 echo " "
-echo " "
 remcli create key --file producerkeys
+echo " "
 echo "THIS IS YOUR PRODUCER KEYS:"
 cat ./producerkeys
 echo "Copy and paste your producer private key:"
 remcli wallet import
 echo " "
-echo " "
 remcli create key --file requestkeys
+echo " "
 echo "THIS IS YOUR REQUEST KEYS:"
 cat ./requestkeys
 echo "Copy and paste your request private key:"
 remcli wallet import
 echo " "
-echo "What's your producer account name?"
-read -e produceraccountname
-echo " "
 echo "What's your active public key?"
 read -e activepublickey
 echo " "
-echo "What's your request public key?"
-read -e requestpublickey
+echo "What's your producer private key?"
+read -e producerprivatekey
 echo " "
 echo "What's your producer public key?"
 read -e producerpublickey
 echo " "
-echo "What's your producer private key?"
-read -e producerprivatekey
+echo "What's your request public key?"
+read -e requestpublickey
+echo " "
+echo "What's your producer account name?"
+read -e produceraccountname
 echo " "
 echo "What's your producer domain address?"
 read -e domain
