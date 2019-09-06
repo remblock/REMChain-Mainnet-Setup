3e072f8a2a428ec590d9e20070517aa74fdcf3df
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 14:47:23 +0930

    Create Install-2.sh

---
+++ Install-2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://github.com/Remmeauth/remprotocol/releases/download/0.1.0/remprotocol_0.1.0-ubuntu-18.04_amd64.deb && sudo apt install ./remprotocol_0.1.0-ubuntu-18.04_amd64.deb
+wget https://testchain.remme.io/genesis.json
+mkdir data && mkdir config
+echo -e "plugin = eosio::chain_api_plugin\n\nplugin = eosio::net_api_plugin\n\nhttp-server-address = 0.0.0.0:8888\n\np2p-listen-endpoint = 0.0.0.0:9876\n\np2p-peer-address = 167.71.88.152:9877\n\nverbose-http-errors = true" > ./config/config.ini
+remnode --config-dir ./config/ --data-dir ./data/ --delete-all-blocks --genesis-json genesis.json
