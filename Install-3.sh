09ab702f83cbb831751ce16a74d4eccf6a9e56ed
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 17:13:40 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -7,7 +7,7 @@ remcli wallet create --file walletpass
 remcli create key --file requestkey
 remcli create key --file producerkey
 cat ./requestkey
-cat ./producerkey
 remcli wallet import
+cat ./producerkey
 remcli wallet import
 rm -f ./Install-2.sh Install-3.sh
