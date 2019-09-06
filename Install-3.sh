7be374c85e0035c81da1e72641c93d8de7cbefe8
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 22:04:15 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -18,10 +18,12 @@ remcli create key --file requestkeys
 cat ./requestkeys
 echo "Copy and paste your request private key:"
 remcli wallet import
-echo "What's your active public key?"
-read -e activepublickey
 echo "What's your producer account name?"
 read -e produceraccountname
+echo "What's your active public key?"
+read -e activepublickey
+echo "What's your request public key?"
+read -e requestpublickey
 echo "What's your producer public key?"
 read -e producerpublickey
 echo "What's your producer private key?"
@@ -31,4 +33,13 @@ read -e domain
 echo -e "plugin = eosio::chain_api_plugin\n\nplugin = eosio::net_api_plugin\n\nhttp-server-address = 0.0.0.0:8888\n\np2p-listen-endpoint = 0.0.0.0:9876\n\np2p-peer-address = 167.71.88.152:9877\n\nverbose-http-errors = true\n\nproducer-name = $produceraccountname\n\nsignature-provider = $producerpublickey=KEY:$producerprivatekey" > ./config/config.ini
 remcli set account permission $produceraccountname active activepublickey owner -p produceraccountname@owner
 remcli system regproducer $produceraccountname $producerpublickey $domain
+remcli set account permission $produceraccountname vote $requestpublickey active -p $produceraccountname@active
+remcli set action permission $produceraccountname rem voteproducer vote -p $produceraccountname@active
+remcli system voteproducer prods $produceraccountname $requestpublickey -p $produceraccountname@vote
+remcli set account permission $produceraccountname claim $requestpublickey active -p $produceraccountname@active
+remcli set action permission $produceraccountname rem claimrewards claim -p $produceraccountname@active
+remcli set account permission $produceraccountname stake $requestpublickey active -p $produceraccountname@active
+remcli set action permission $produceraccountname rem delegatebw stake -p $produceraccountname@active
+remcli set account permission $produceraccountname transfer $requestpublickey active -p $produceraccountname@active
+remcli set action permission $produceraccountname rem transfer transfer -p $produceraccountname@active
 rm -f ./Install-2.sh Install-3.sh
