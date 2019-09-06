0e846fd7d0bccd95ea935882f5d5d78646fa6879
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-06 17:51:02 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -10,4 +10,11 @@ cat ./requestkey
 remcli wallet import
 cat ./producerkey
 remcli wallet import
+echo "What's your account name?"
+read -e produceraccountname
+echo "What's your producer private key?"
+read -e producerprivatekey
+echo "What's your producer public key?"
+read -e producerpublickey
+echo -e "producer-name = $produceraccountname\n\nsignature-provider = $producerpublickey=KEY:$producerprivatekey" > ./config/config.ini
 rm -f ./Install-2.sh Install-3.sh
