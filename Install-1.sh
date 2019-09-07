fccd137c6146d88a5f40f2d56fabec575511014c
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-07 14:11:41 +0930

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt update && sudo apt upgrade
+echo " "
+echo " "
+echo "SETUP NEW ROOT PASSWORD:"
 passwd root
 sed -i '/^#Port 22/s/#Port 22/Port 3984/' /etc/ssh/sshd_config && sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 sudo apt-get install ufw -y
@@ -14,6 +17,9 @@ sudo apt -y install fail2ban
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 sudo service sshd restart
+echo " "
+echo " "
+echo "SETUP NEW USER ACCOUNT:"
 adduser admin
 usermod -aG sudo admin
 rm -f ./Install-1.sh 
