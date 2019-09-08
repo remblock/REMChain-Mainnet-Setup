cc45d967320208cfb68ce972792deacfd889e771
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-08 19:55:59 +0930

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -27,4 +27,4 @@ read username
 adduser $username
 usermod -aG sudo $username
 rm -f ./Install-1.sh 
-su "$username" sh -c "sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-2.sh && sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-3.sh && sudo chmod u+x Install-2.sh && sudo chmod u+x Install-3.sh && sudo ./Install-2.sh"
+sudo -A su "$username" sh -c "sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-2.sh && sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-3.sh && sudo chmod u+x Install-2.sh && sudo chmod u+x Install-3.sh && sudo ./Install-2.sh"
