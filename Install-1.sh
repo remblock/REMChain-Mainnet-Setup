705c1fbce2b304960206e8c5a10b904c1f2f9108
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-08 20:11:35 +0930

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -27,4 +27,5 @@ read username
 adduser $username
 usermod -aG sudo $username
 rm -f ./Install-1.sh 
-su "$username" sh -c "sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-2.sh && sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-3.sh && sudo chmod u+x Install-2.sh && sudo chmod u+x Install-3.sh && sudo ./Install-2.sh"
+export SUDO_ASKPASS=/usr/lib/openssh/gnome-ssh-askpass
+sudo -A su "$username" sh -c "sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-2.sh && sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-3.sh && sudo chmod u+x Install-2.sh && sudo chmod u+x Install-3.sh && sudo ./Install-2.sh"
