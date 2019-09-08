e2740966cf657da8268ebaee2ee912f643f84b8d
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-08 22:02:50 +0930

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade
+sudo -S apt update && sudo -S apt upgrade
 echo " "
 echo "SET YOUR NEW ROOT PASSWORD:"
 passwd root
 echo " "
 echo "CHOOSE A RANDOM 5 DIGIT PORT NUMBER (IF UNSURE USE 39844):"
 read -n 5 portnumber
-sudo sed -i "/^#Port 22/s/#Port 22/Port $portnumber/" /etc/ssh/sshd_config && sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
-sudo apt-get install ufw -y
-sudo ufw allow ssh/tcp
-sudo ufw limit ssh/tcp
-sudo ufw allow 3984/tcp
-sudo ufw allow 8888/tcp
-sudo ufw logging on
-sudo ufw enable
-sudo apt -y install fail2ban
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
-sudo service sshd restart
+sudo -S sed -i "/^#Port 22/s/#Port 22/Port $portnumber/" /etc/ssh/sshd_config && sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+sudo -S apt-get install ufw -y
+sudo -S ufw allow ssh/tcp
+sudo -S ufw limit ssh/tcp
+sudo -S ufw allow 3984/tcp
+sudo -S ufw allow 8888/tcp
+sudo -S ufw logging on
+sudo -S ufw enable
+sudo -S apt -y install fail2ban
+sudo -S systemctl enable fail2ban
+sudo -S systemctl start fail2ban
+sudo -S service sshd restart
 echo " "
 echo " "
 echo "CREATING YOUR NEW USER ACCOUNT"
 echo "SET YOUR USERNAME:"
 read username
 adduser $username
-usermod -aG sudo $username
+usermod -aG sudo -S $username
 rm -f ./Install-1.sh 
 su "$username" sh -c "sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-2.sh && sudo wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-3.sh && sudo chmod u+x Install-2.sh && sudo chmod u+x Install-3.sh && sudo ./Install-2.sh"
