1ad566a7858bb6135431ded67809d41d95130024
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-10 04:26:10 +0930

    Delete Install-3.sh

--- Install-3.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo -S wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-4.sh && sudo -S chmod u+x Install-4.sh && sudo -S ./Install-4.sh"
-rm -f ./Install-3.sh
