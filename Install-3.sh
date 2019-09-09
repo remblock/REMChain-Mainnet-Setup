d862501f0bc2f00e6f967c98c9288690a82aea25
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-09 17:20:51 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo "YOUR USERNAME:"
-read username
+username=$(cat username.txt)
 sudo su "$username" sh -c "sudo -S wget https://github.com/SooSDExZ/REMChain-Testnet-Guide/raw/master/Install-4.sh && sudo -S chmod u+x Install-4.sh && sudo -S ./Install-4.sh"
 rm -f ./Install-3.sh
