64a1c86941cef37018eedf35b8662ebe20cb5e1f
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-10 17:13:14 +0930

    Update Install-2.sh

--- Install-2.sh
+++ Install-2.sh
@@ -65,13 +65,13 @@ echo -e "plugin = eosio::chain_api_plugin\n\nplugin = eosio::net_api_plugin\n\nh
 remcli set account permission $produceraccountname active $activepublickey owner -p $produceraccountname@owner
 remcli system regproducer $produceraccountname $producerpublickey $domain
 remcli set account permission $produceraccountname vote $requestpublickey active -p $produceraccountname@active
-remcli set action permission $produceraccountname rem voteproducer NULL -p $produceraccountname@active
+remcli set action permission $produceraccountname rem voteproducer vote -p $produceraccountname@active
 remcli set account permission $produceraccountname claim $requestpublickey active -p $produceraccountname@active
-remcli set action permission $produceraccountname rem claimrewards NULL -p $produceraccountname@active
+remcli set action permission $produceraccountname rem claimrewards claim -p $produceraccountname@active
 remcli set account permission $produceraccountname stake $requestpublickey active -p $produceraccountname@active
-remcli set action permission $produceraccountname rem delegatebw NULL -p $produceraccountname@active
+remcli set action permission $produceraccountname rem delegatebw stake -p $produceraccountname@active
 remcli set account permission $produceraccountname transfer $requestpublickey active -p $produceraccountname@active
-remcli set action permission $produceraccountname rem transfer NULL -p $produceraccountname@active
+remcli set action permission $produceraccountname rem transfer transfer -p $produceraccountname@active
 remcli system voteproducer prods $produceraccountname $requestpublickey -p $produceraccountname@vote
 walletpassword=$(cat walletpass)
 echo $walletpassword > producerwalletpass.txt
