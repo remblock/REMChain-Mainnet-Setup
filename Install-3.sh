17137fe4481e600ba5f8bb3d983e5e4d717c2124
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-11 14:18:51 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -6,6 +6,7 @@ produceraccountname=$(cat produceraccountname.txt)
 activepublickey=$(head -n 2 key1 | tail -1)
 requestpublickey=$(head -n 2 key2 | tail -1)
 producerpublickey=$(head -n 2 key3 | tail -1)
+remcli system regproducer $produceraccountname $producerpublickey $domain
 remcli set account permission $produceraccountname produce $producerpublickey active -p $produceraccountname@active
 remcli set account permission $produceraccountname vote $requestpublickey active -p $produceraccountname@active
 remcli set account permission $produceraccountname claim $requestpublickey active -p $produceraccountname@active
@@ -16,7 +17,6 @@ remcli set action permission $produceraccountname rem voteproducer vote -p $prod
 remcli set action permission $produceraccountname rem claimrewards claim -p $produceraccountname@active
 remcli set action permission $produceraccountname rem delegatebw stake -p $produceraccountname@active
 remcli set action permission $produceraccountname rem.token transfer transfer -p $produceraccountname@active
-remcli system regproducer $produceraccountname $producerpublickey $domain
 remcli system voteproducer prods $produceraccountname $produceraccountname -p $produceraccountname@vote
 walletpassword=$(cat walletpass)
 echo $walletpassword > producerwalletpass.txt
