827686c414ea35f4458376001f4ffeee91fd6f8d
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-11 19:57:40 +0930

    Update Install-4.sh

--- Install-4.sh
+++ Install-4.sh
@@ -24,4 +24,4 @@ producerwalletpass=$(cat producerwalletpass.txt)
 remcli wallet remove_key $ownerpublickey --password=$producerwalletpass
 remcli wallet remove_key $activepublickey --password=$producerwalletpass
 rm key1 key2 key3 activekeys domain.txt ownerpublickey.txt produceraccountname.txt producerwalletpass.txt
-rm -f ./Install-3.sh
+rm -f ./Install-4.sh
