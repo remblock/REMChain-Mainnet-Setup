891482dad4bb70c91dc96988e5748184269d8a0c
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-12 00:58:57 +0930

    Update Install-4.sh

--- Install-4.sh
+++ Install-4.sh
@@ -18,7 +18,4 @@ producerwalletpass=$(cat producerwalletpass.txt)
 remcli wallet remove_key $ownerpublickey --password=$producerwalletpass
 remcli wallet remove_key $activepublickey --password=$producerwalletpass
 rm key1 key2 key3 activekeys domain.txt ownerpublickey.txt produceraccountname.txt producerwalletpass.txt
-sudo killall remnode
-sudo remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
-sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 rm -f ./Install-4.sh
