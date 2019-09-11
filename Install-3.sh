d37331cbf7b3c4d69ad6b10d1bfab32ec7f913df
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-11 19:58:55 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 domain=$(cat domain.txt)
-produceraccountname=$(cat produceraccountname.txt)
 producerpublickey=$(head -n 2 key3 | tail -1)
+produceraccountname=$(cat produceraccountname.txt)
 remcli system regproducer $produceraccountname $producerpublickey $domain
 rm -f ./Install-3.sh
