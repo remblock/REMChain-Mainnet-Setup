1cdd3c140b8a68e960c426164dfb92585451cd55
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-12 01:04:03 +0930

    Delete Install-5.sh

--- Install-5.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo killall remnode
-sudo remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
-sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
-rm -f ./Install-5.sh
