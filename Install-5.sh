d637dfd0a442642e8b2f4e7a0d3c8084370270d6
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-12 00:58:19 +0930

    Update Install-5.sh

--- Install-5.sh
+++ Install-5.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo killall remnode
 sudo remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
 sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
