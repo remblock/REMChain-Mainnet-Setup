fd01a63a722206cfa02017003c8bb392c12d5f46
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-14 00:53:37 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -5,7 +5,7 @@ ownerpublickey=$(cat ownerpublickey.txt)
 requestpublickey=$(head -n 2 key2 | tail -1)
 transferpublickey=$(head -n 2 key3 | tail -1)
 produceraccountname=$(cat produceraccountname.txt)
-remcli system regproducer $produceraccountname $producerpublickey $domain
+remcli system regproducer $produceraccountname $requestpublickey $domain
 remcli set account permission $produceraccountname safemode $ownerpublickey owner -p $produceraccountname@owner
 remcli set account permission $produceraccountname vote $requestpublickey active -p $produceraccountname@active
 remcli set account permission $produceraccountname claim $requestpublickey active -p $produceraccountname@active
