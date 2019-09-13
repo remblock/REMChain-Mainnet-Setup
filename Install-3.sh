1f2c7ff9aa469145414ded497d9b10f10e1fe199
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-13 16:55:33 +0930

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -2,12 +2,12 @@
 
 domain=$(cat domain.txt)
 requestpublickey=$(head -n 2 key2 | tail -1)
-producerpublickey=$(head -n 2 key3 | tail -1)
+transferpublickey=$(head -n 2 key3 | tail -1)
 produceraccountname=$(cat produceraccountname.txt)
-remcli set account permission $produceraccountname produce $producerpublickey active -p $produceraccountname@active
+remcli set account permission $produceraccountname produce $requestpublickey active -p $produceraccountname@active
 remcli system regproducer $produceraccountname $producerpublickey $domain
 remcli set account permission $produceraccountname vote $requestpublickey active -p $produceraccountname@active
 remcli set account permission $produceraccountname claim $requestpublickey active -p $produceraccountname@active
 remcli set account permission $produceraccountname stake $requestpublickey active -p $produceraccountname@active
-remcli set account permission $produceraccountname transfer $requestpublickey active -p $produceraccountname@active
+remcli set account permission $produceraccountname transfer $transferpublickey active -p $produceraccountname@active
 rm -f ./Install-3.sh
