8adcc226b162815e6e885b1ef89c640b359078ba
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-15 21:57:13 +0930

    Update Install-4.sh

--- Install-4.sh
+++ Install-4.sh
@@ -2,17 +2,17 @@
 
 domain=$(cat domain.txt)
 ownerpublickey=$(cat ownerpublickey.txt)
+owneraccountname=$(cat owneraccountname.txt)
 activepublickey=$(head -n 2 key1 | tail -1)
 requestpublickey=$(head -n 2 key2 | tail -1)
-produceraccountname=$(cat produceraccountname.txt)
 remcli system regproducer $produceraccountname $requestpublickey $domain
-remcli set action permission $produceraccountname rem regproducer safemode -p $produceraccountname@owner
-remcli set action permission $produceraccountname rem unregprod safemode -p $produceraccountname@owner
-remcli set action permission $produceraccountname rem voteproducer vote -p $produceraccountname@active
-remcli set action permission $produceraccountname rem claimrewards claim -p $produceraccountname@active
-remcli set action permission $produceraccountname rem delegatebw stake -p $produceraccountname@active
-remcli set action permission $produceraccountname rem.token transfer transfer -p $produceraccountname@active
-remcli system voteproducer prods $produceraccountname $produceraccountname -p $produceraccountname@vote
+remcli set action permission $owneraccountname rem regproducer safemode -p $owneraccountname@owner
+remcli set action permission $owneraccountname rem unregprod safemode -p $owneraccountname@owner
+remcli set action permission $owneraccountname rem voteproducer vote -p $owneraccountname@active
+remcli set action permission $owneraccountname rem claimrewards claim -p $owneraccountname@active
+remcli set action permission $owneraccountname rem delegatebw stake -p $owneraccountname@active
+remcli set action permission $owneraccountname rem.token transfer transfer -p $owneraccountname@active
+remcli system voteproducer prods $owneraccountname $owneraccountname -p $owneraccountname@vote
 walletpassword=$(cat walletpass)
 echo $walletpassword > producerwalletpass.txt
 producerwalletpass=$(cat producerwalletpass.txt)
@@ -21,4 +21,4 @@ remcli wallet remove_key $activepublickey --password=$producerwalletpass
 sudo killall remnode
 sudo remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
 sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
-rm key1 key2 key3 activekeys Install-4.sh domain.txt ownerpublickey.txt produceraccountname.txt producerwalletpass.txt 
+rm key1 key2 key3 key4 key5 activekeys1 activekeys2 activekeys3 Install-4.sh domain.txt ownerpublickey.txt owneraccountname.txt producerwalletpass.txt 
