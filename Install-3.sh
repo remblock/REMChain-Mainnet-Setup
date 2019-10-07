4df1073d15a883979f446b4f77271b43e6ee65e2
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-10-07 18:18:40 +1030

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -10,15 +10,15 @@ owneraccountname=$(cat owneraccountname.txt)
 requestpublickey=$(head -n 2 key4 | tail -1)
 transferpublickey=$(head -n 2 key5 | tail -1)
 
-#---------------------------------------
+#----------------------------------------------
 #REGISTERING AS NEW BLOCK PRODUCER 
-#---------------------------------------
+#----------------------------------------------
 
 remcli system regproducer $owneraccountname $requestpublickey $domain
 
-#---------------------------------------
+#----------------------------------------------
 #CREATING REMCHAIN ACCOUNT PERMISSIONS 
-#---------------------------------------
+#----------------------------------------------
 
 remcli set account permission $owneraccountname safemode $ownerpublickey owner -p $owneraccountname@owner
 remcli set account permission $owneraccountname vote $requestpublickey active -p $owneraccountname@active
