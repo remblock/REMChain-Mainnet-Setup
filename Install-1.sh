b7480b58288d49e671534423055472d162d930dd
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-10-21 13:06:53 +1030

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -4,6 +4,8 @@
 #                INSTALL-1.SH                  #
 #**********************************************#
 
+conf_path="/root/remblock/autobot/config"
+
 function pause(){
    read -p "$*"
 }
@@ -22,9 +24,9 @@ passwd root
 #----------------------------------------------
 
 echo " "
-#echo "CHOOSE A RANDOM 5 DIGIT PORT NUMBER (IF UNSURE USE 39844):"
-#read -n 5 portnumber
-sudo -S sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+echo "CHOOSE A RANDOM 5 DIGIT PORT NUMBER (IF UNSURE USE 39844):"
+read -n 5 portnumber
+sudo -S sed -i "/^#Port 22/s/#Port 22/Port $portnumber/" /etc/ssh/sshd_config && sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 
 #----------------------------------------------
 # INSTALLING UNCOMPLICATED FIREWALL
@@ -33,6 +35,7 @@ sudo -S sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 sudo -S apt-get install ufw -y
 sudo -S ufw allow ssh/tcp
 sudo -S ufw limit ssh/tcp
+sudo -S ufw allow $portnumber/tcp
 sudo -S ufw allow 8888/tcp
 sudo -S ufw allow 9877/tcp
 sudo -S ufw logging on
@@ -69,10 +72,23 @@ sudo sed -i ‘s/PasswordAuthentication yes/PasswordAuthentication no/’ /etc/s
 
 echo " " 
 echo "TAKE NOTE OF YOUR SSH PRIVATE KEY:" 
-sudo cat ./home/$username/.ssh/id_rsa 
-#echo " " 
+sudo cat /home/$username/.ssh/id_rsa 
+
+echo " " 
 pause 'Press [Enter] key to continue...' 
-#echo " " 
+echo " " 
+
+#----------------------------------------------
+#COPY SSH KEY TO LOCAL MACHINE  
+#----------------------------------------------
+
+echo " let's copy the key file "
+
+echo $SSH_CLIENT | awk '{ print $1}' 
+ip_ssh=$(echo $SSH_CLIENT | awk '{ print $1}')
+sudo ssh-copy-id $username@$ip_ssh
+echo " "
+echo "********************** Done ************************"
 
 #----------------------------------------------
 # INSTALLING CANONICAL LIVEPATCH SERVICE
@@ -107,6 +123,8 @@ echo -e "plugin = eosio::chain_api_plugin\n\nplugin = eosio::net_api_plugin\n\nh
 #----------------------------------------------
 # THE INITIAL RUN OF THE REMNODE
 #----------------------------------------------
+
+remnode --config-dir ./config/ --data-dir ./data/ --delete-all-blocks --genesis-json genesis.json  2>&1 | tee remnode_sync.log
 
 t1=""
 t2=""
@@ -147,7 +165,9 @@ sleep 2
 remcli wallet create --file walletpass
 walletpassword=$(cat walletpass)
 echo $walletpassword > producerwalletpass.txt
-
+echo "owneraccountname="$username >>$conf_path
+echo "walletpassword="$walletpassword >>$conf_path
+echo " "
 #----------------------------------------------
 # ASKING FOR USER DETAILS 
 #----------------------------------------------
@@ -190,7 +210,7 @@ cp key1 activekeys1
 sudo -S sed -i "/^Private key: /s/Private key: //" key1 && sudo -S sed -i "/^Public key: /s/Public key: //" key1
 activepublickey1=$(head -n 2 key1 | tail -1)
 activeprivatekey1=$(head -n 1 key1 | tail -1)
-activeproducername1=a$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 11 | head -n 1 |  grep -o . | sort |tr -d "\n")
+activeproducername1=$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 12 | head -n 1 |  grep -o . | sort |tr -d "\n")
 echo $activeproducername1 > activeproducername1.txt
 activeproducername1=$(cat activeproducername1.txt)
 remcli wallet import --private-key=$activeprivatekey1
@@ -211,7 +231,7 @@ cp key2 activekeys2
 sudo -S sed -i "/^Private key: /s/Private key: //" key2 && sudo -S sed -i "/^Public key: /s/Public key: //" key2
 activepublickey2=$(head -n 2 key2 | tail -1)
 activeprivatekey2=$(head -n 1 key2 | tail -1)
-activeproducername2=b$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 11 | head -n 1 | grep -o . | sort |tr -d "\n")
+activeproducername2=$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 12 | head -n 1 | grep -o . | sort |tr -d "\n")
 echo $activeproducername2 > activeproducername2.txt
 activeproducername2=$(cat activeproducername2.txt)
 remcli wallet import --private-key=$activeprivatekey2
@@ -232,7 +252,7 @@ cp key3 activekeys3
 sudo -S sed -i "/^Private key: /s/Private key: //" key3 && sudo -S sed -i "/^Public key: /s/Public key: //" key3
 activepublickey3=$(head -n 2 key3 | tail -1)
 activeprivatekey3=$(head -n 1 key3 | tail -1)
-activeproducername3=c$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 11 | head -n 1 |  grep -o . | sort |tr -d "\n")
+activeproducername3=$(cat /dev/urandom | tr -dc 'a-z1-5' | fold -w 12 | head -n 1 |  grep -o . | sort |tr -d "\n")
 echo $activeproducername3 > activeproducername3.txt
 activeproducername3=$(cat activeproducername3.txt)
 remcli wallet import --private-key=$activeprivatekey3
