cc68dd47c237bc2fade81d6402cca4718fbcd852
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-10-23 21:27:49 +1030

    Update and rename Install-4.sh to Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #**********************************************#
-#                 INSTALL-4.SH                 #
+#                 INSTALL-3.SH                 #
 #**********************************************#
 
 domain=$(cat domain.txt)
@@ -51,4 +51,4 @@ sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 # REMOVING FILES WHICH AINT REQUIRED ANYMORE
 #----------------------------------------------
 
-rm key1 key2 key3 key4 key5 activekeys1 activekeys2 activekeys3 walletpass requestkeys transferkeys countdown.sh Install-1.sh Install-2.sh Install-3.sh Install-4.sh domain.txt ownerpublickey.txt owneraccountname.txt activeproducername1.txt activeproducername2.txt activeproducername3.txt producerwalletpass.txt
+rm key1 key2 key3 key4 key5 activekeys1 activekeys2 activekeys3 walletpass requestkeys transferkeys countdown.sh Install-1.sh Install-2.sh Install-3.sh domain.txt ownerpublickey.txt owneraccountname.txt activeproducername1.txt activeproducername2.txt activeproducername3.txt producerwalletpass.txt
