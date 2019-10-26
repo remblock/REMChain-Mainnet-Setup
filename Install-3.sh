1988e7ba2656643e4a4a9b381b3c4469a9f9f586
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-10-26 20:33:16 +1030

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#**********************************************#
-#                 INSTALL-3.SH                 #
-#**********************************************#
+#****************************************************************************************************#
+#                                             INSTALL-3.SH                                           #
+#****************************************************************************************************#
 
 domain=$(cat domain.txt)
 ownerpublickey=$(cat ownerpublickey.txt)
@@ -13,9 +13,9 @@ activepublickey2=$(head -n 2 key2 | tail -1)
 activepublickey3=$(head -n 2 key3 | tail -1)
 requestpublickey=$(head -n 2 key4 | tail -1)
 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 # CREATING REMCHAIN KEY PERMISSIONS 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 remcli set action permission $owneraccountname rem regproducer safemode -p $owneraccountname@owner
 remcli set action permission $owneraccountname rem unregprod safemode -p $owneraccountname@owner
@@ -24,31 +24,31 @@ remcli set action permission $owneraccountname rem claimrewards claim -p $ownera
 remcli set action permission $owneraccountname rem delegatebw stake -p $owneraccountname@active
 remcli set action permission $owneraccountname rem.token transfer transfer -p $owneraccountname@active
 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 # CASTING YOUR VOTES TO YOURSELF
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 remcli system voteproducer prods $owneraccountname $owneraccountname -p $owneraccountname@vote
 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 # REMOVING OWNER AND ACTIVE KEYS
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 remcli wallet remove_key $ownerpublickey --password=$producerwalletpass
 remcli wallet remove_key $activepublickey1 --password=$producerwalletpass
 remcli wallet remove_key $activepublickey2 --password=$producerwalletpass
 remcli wallet remove_key $activepublickey3 --password=$producerwalletpass
 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 # JUMP STARTING THE REMNODE
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 sudo killall remnode
 sudo remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
 sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 # REMOVING FILES WHICH AINT REQUIRED ANYMORE
-#----------------------------------------------
+#-----------------------------------------------------------------------------------------------------
 
 rm key1 key2 key3 key4 key5 activekeys1 activekeys2 activekeys3 walletpass requestkeys transferkeys countdown.sh Install-1.sh Install-2.sh Install-3.sh domain.txt ownerpublickey.txt owneraccountname.txt activeproducername1.txt activeproducername2.txt activeproducername3.txt producerwalletpass.txt
