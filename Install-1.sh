e538577c32c3ae5ae4b43e7f4faf7ec4cdc44e1d
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-10-27 00:35:12 +1030

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -95,7 +95,7 @@ read -p "PLEASE ENTER YOUR LOCAL HOST USERNAME: " ssh_host_user
 echo " "
 read -p "PLEASE ENTER YOUR LOCAL HOST PASSWORD: " ssh_host_password
 echo " "
-sudo sshpass -p $ssh_host_password ssh-copy-id -i /home/$username/.ssh/id_rsa $ssh_host_user@$ip_ssh -p $ssh_host_port
+sudo cat ~/.ssh/id_rsa.pub | sshpass -p $ssh_host_password ssh $ssh_host_user@$ip_ssh -p $ssh_host_port "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
 echo " "
 echo "[********************** DONE ************************]"
 }
