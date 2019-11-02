605c24ec9f414a3f327f7503f137ded1a70d1dbc
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-11-03 00:35:11 +1030

    Update Install-3.sh

--- Install-3.sh
+++ Install-3.sh
@@ -52,3 +52,5 @@ sudo remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 #-----------------------------------------------------------------------------------------------------
 
 rm key1 key2 key3 key4 key5 activekeys1 activekeys2 activekeys3 walletpass requestkeys transferkeys Install-1.sh Install-2.sh Install-3.sh domain.txt ownerpublickey.txt owneraccountname.txt activeproducername.txt producerwalletpass.txt
+
+printf "\n[********************** COMPLETED ************************]\n\n"
