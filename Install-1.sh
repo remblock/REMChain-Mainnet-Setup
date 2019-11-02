5d9d98a6488a8b99497da41e903528a70c27b8bd
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-11-02 23:16:43 +1030

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -350,17 +350,17 @@ echo " "
 # CREATING YOUR REMCHAIN ACCOUNTS
 #-----------------------------------------------------------------------------------------------------
 
-echo "CREATING ACTIVE ACCOUNT NAME 1:"
+echo "[********************** CREATING ACTIVE ACCOUNT 1 ************************]"
 echo " "
 remcli system newaccount $owneraccountname $activeproducername1 $activepublickey1 $activepublickey1 -x 120 --transfer --stake "100.0000 REM" -p $owneraccountname@owner
 pause 'Press [Enter] key to continue...'
 echo " "
-echo "CREATING ACTIVE ACCOUNT NAME 2:"
+echo "[********************** CREATING ACTIVE ACCOUNT 2 ************************]"
 echo " "
 remcli system newaccount $owneraccountname $activeproducername2 $activepublickey2 $activepublickey2 -x 120 --transfer --stake "100.0000 REM" -p $owneraccountname@owner
 pause 'Press [Enter] key to continue...'
 echo " "
-echo "CREATING ACTIVE ACCOUNT NAME 3:"
+echo "[********************** CREATING ACTIVE ACCOUNT 3 ************************]"
 echo " "
 remcli system newaccount $owneraccountname $activeproducername3 $activepublickey3 $activepublickey3 -x 120 --transfer --stake "100.0000 REM" -p $owneraccountname@owner
 echo " "
