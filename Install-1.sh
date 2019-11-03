109ae97ba463bd8545552c8a614dd202195c5aeb
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-11-03 17:23:42 +1030

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -152,6 +152,7 @@ done
 # RUNNING REMNODE IN THE BACKGROUND
 #-----------------------------------------------------------------------------------------------------
 
+remnode --config-dir ./config/ --data-dir ./data/ --fix-reversible-blocks --force-all-checks --genesis-json genesis.json
 remnode --config-dir ./config/ --data-dir ./data/ >> remnode.log 2>&1 &
 sleep 1
 
